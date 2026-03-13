33d23b5af414221a265143d401a56305f49a5bee
shanerutter-gif <shanerutter@gmail.com>
2026-03-12 20:20:59 -0700

    Update MainViewController.swift

--- ios/App/App/MainViewController.swift
+++ ios/App/App/MainViewController.swift
@@ -1,10 +1,38 @@
 import UIKit
-import Capacitor
+import WebKit
 
-class MainViewController: CAPBridgeViewController {
-    override func instanceDescriptor() -> InstanceDescriptor {
-        let descriptor = super.instanceDescriptor()
-        descriptor.serverURL = URL(string: "https://spotd.biz")
-        return descriptor
+class MainViewController: UIViewController, WKNavigationDelegate {
+
+    var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0.96, green: 0.94, blue: 0.90, alpha: 1.0)
+
+        let config = WKWebViewConfiguration()
+        config.allowsInlineMediaPlayback = true
+        config.mediaTypesRequiringUserActionForPlayback = []
+
+        webView = WKWebView(frame: view.bounds, configuration: config)
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        webView.navigationDelegate = self
+        webView.backgroundColor = UIColor(red: 0.96, green: 0.94, blue: 0.90, alpha: 1.0)
+        webView.isOpaque = false
+        view.addSubview(webView)
+
+        if let url = URL(string: "https://spotd.biz") {
+            webView.load(URLRequest(url: url))
+        }
+    }
+
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let url = navigationAction.request.url,
+           let host = url.host,
+           !host.contains("spotd.biz") {
+            UIApplication.shared.open(url)
+            decisionHandler(.cancel)
+        } else {
+            decisionHandler(.allow)
+        }
     }
 }
