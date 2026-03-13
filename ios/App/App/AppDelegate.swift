ed92386cbe1ce5356bb1b3ea3b2896f57d84c315
shanerutter-gif <shanerutter@gmail.com>
2026-03-12 21:07:55 -0700

    Update AppDelegate.swift

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -18,21 +18,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WKNavigationDelegate {
         config.allowsInlineMediaPlayback = true
         config.mediaTypesRequiringUserActionForPlayback = []
 
-        webView = WKWebView(frame: vc.view.bounds, configuration: config)
-        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        webView = WKWebView(frame: .zero, configuration: config)
+        webView.translatesAutoresizingMaskIntoConstraints = false
         webView.navigationDelegate = self
         webView.backgroundColor = UIColor(red: 0.96, green: 0.94, blue: 0.90, alpha: 1.0)
         webView.isOpaque = true
+        webView.scrollView.contentInsetAdjustmentBehavior = .never
+
         vc.view.addSubview(webView)
+
+        let safeArea = vc.view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: vc.view.bottomAnchor),
+            webView.leadingAnchor.constraint(equalTo: vc.view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: vc.view.trailingAnchor)
+        ])
 
         if let url = URL(string: "https://spotd.biz") {
             webView.load(URLRequest(url: url))
         }
 
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+
         window!.rootViewController = vc
         window!.makeKeyAndVisible()
 
         return true
+    }
+
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            webView.scrollView.contentInset.bottom = keyboardFrame.height
+        }
+    }
+
+    @objc func keyboardWillHide(_ notification: Notification) {
+        webView.scrollView.contentInset.bottom = 0
     }
 
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
